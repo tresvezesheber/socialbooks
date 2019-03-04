ab72253d47b8cd25739657858d2058443abaa36d
Heber dos Santos Soares <hebireaper@gmail.com>
2019-03-04 20:31:29 -0300

    ResponseEntity 201 para salvar e 404 na busca

--- src/main/java/com/algaworks/socialbooks/resources/LivrosResources.java
+++ src/main/java/com/algaworks/socialbooks/resources/LivrosResources.java
@@ -2,9 +2,15 @@ package com.algaworks.socialbooks.resources;
 
 import com.algaworks.socialbooks.domain.Livro;
 import com.algaworks.socialbooks.repository.LivrosRepository;
+import com.sun.xml.internal.bind.v2.TODO;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import javax.xml.ws.Response;
+import java.net.URI;
 import java.util.List;
 import java.util.Optional;
 
@@ -22,13 +28,25 @@ public class LivrosResources {
     }
 
     @PostMapping
-    public void salvar(@RequestBody Livro livro) {
-        livrosRepository.save(livro);
+    public ResponseEntity<Void> salvar(@RequestBody Livro livro) {
+
+        livro = livrosRepository.save(livro);
+
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(livro.getId()).toUri();
+
+        return ResponseEntity.created(uri).build();
     }
 
     @GetMapping("/{id}")
-    public Optional<Livro> buscar(@PathVariable Long id) {
-        return livrosRepository.findById(id);
+    public ResponseEntity<?> buscar(@PathVariable Long id) {
+        Optional<Livro> livroOptional = livrosRepository.findById(id);
+
+        //TODO improve this verification
+        if (!livroOptional.isPresent()){
+            return ResponseEntity.notFound().build();
+        }
+
+        return ResponseEntity.status(HttpStatus.OK).body(livroOptional);
     }
 
     @DeleteMapping("/{id}")
