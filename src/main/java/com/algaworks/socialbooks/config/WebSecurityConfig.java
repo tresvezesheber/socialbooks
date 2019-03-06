93b20387b3aab62e0a89b3a165fca6568a7a45e0
Heber dos Santos Soares <hebireaper@gmail.com>
2019-03-06 13:26:01 -0300

    permitAll a partir da raiz /**

--- src/main/java/com/algaworks/socialbooks/config/WebSecurityConfig.java
+++ src/main/java/com/algaworks/socialbooks/config/WebSecurityConfig.java
@@ -23,7 +23,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests()
-                .antMatchers("/h2-console/**").permitAll()
+                .antMatchers("/h2-console/**", "/**").permitAll()
                 .antMatchers(HttpMethod.OPTIONS, "/**").permitAll()
                 .anyRequest()
                 .authenticated()
